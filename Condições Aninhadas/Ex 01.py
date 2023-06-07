5a206086b244e9de8d443918578c60a0c33e299b
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-06-06 23:38:35 -0300

    update

--- Condições Aninhadas/Ex 01.py
+++ Condições Aninhadas/Ex 01.py
@@ -3,11 +3,11 @@ print('\033[1:33:40mSIMULADOR DE EMPRÉSTIMO BANCÁRIO\033[m')
 casa = int(input('Insira o valor da casa desejada: R$'))
 anos = int(input('Em quantos anos deseja pagar ? '))
 salario = float(input('Qual sua renda mensal? R$'))
-
-
 prestacao = casa/(anos*12)
-print(f'Para quitar um imóvel de R${casa} em {anos}anos a prestação ficaria em R${prestacao:.2f}')
+
 if prestacao <= salario*0.3:
-    print('Empréstimo aprovado !!')
+    print('\033[1:30:47mEmpréstimo aprovado !!\033[m')
+    print(
+        f'\033[1:30:47mPara quitar um imóvel de R${casa} em {anos} anos a prestação ficaria em R${prestacao:.2f}\033[m')
 else:
-    print('Empréstimo negado pela política de credito!')
+    print('\033[1:30:47mEmpréstimo negado pela política de credito!\033[m')
