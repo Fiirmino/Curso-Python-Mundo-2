4056bbeca87ec623f136902b521eff30b9823708
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-06-18 12:38:31 -0300

    update2.1

--- Repetições (For)/Ex 08 - Números primos.py
+++ Repetições (For)/Ex 08 - Números primos.py
@@ -1,3 +1,19 @@
+num = int (input('Digite um número inteiro: '))
+qnt_primos = 0
+lista = []
+for c in range (1,num+1):
+    primos = num%c == 0
+    if primos:
+        qnt_primos = qnt_primos+1
+        lista.append(c)
+    else:
+        n_primos = lista
+if qnt_primos == 2:
+    print(f'\n{num} é um número primo. Pois ele é divisível por \033[33m{lista}\033[m')
+else:
+    print(f'{num} não é um número primo. Pois ele é divisível por \033[33m{n_primos}\033[m')
+
+
 
 
 
