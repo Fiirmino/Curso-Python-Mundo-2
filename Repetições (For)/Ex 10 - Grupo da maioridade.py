2007397d19a9e19115fb5c584a19501f6c27e903
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-06-22 20:46:31 -0300

    update10

--- Repetições (For)/Ex 10 - Grupo da maioridade.py
+++ Repetições (For)/Ex 10 - Grupo da maioridade.py
@@ -4,17 +4,12 @@ tot = 0
 for c in range(1,8):
     idade = input('Informe sua data de nascimento: ')
     nascimento = datetime.strptime(idade,'%d/%m/%Y')
-    maioridade = hoje.year-nascimento.year
-    if maioridade>=18:
-        tot +=1
-        print('De maior')
-        if hoje.month < nascimento.month or hoje.month == nascimento.month and hoje.day < nascimento.day:
-            maioridade = (hoje.year - nascimento.year) - 1
-            print(f'Você possui {maioridade} anos, logo não é de maior')
+    if 
+        maioridade = (hoje.year-nascimento.year)-1
     else:
-        print(f'Você possui {maioridade} anos, logo não é de maior')
-print(f'o total de pessoas de maior são ')
-
-
-
-
+        maioridade = (hoje.year-nascimento.year)
+    if maioridade >=18:
+        tot+=1
+        print(f'Você é de maior,pois possui {maioridade} anos !')
+    else:
+        print(f'Você é de menor, pois possui {maioridade} anos!')
