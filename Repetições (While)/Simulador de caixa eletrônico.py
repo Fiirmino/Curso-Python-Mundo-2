397df38425b12c75b7cf56171358a794f0cf5caa
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-07-03 23:10:26 -0300

    update final do curso python2
    espero vocês no python3

--- Repetições (While)/Simulador de caixa eletrônico.py
+++ Repetições (While)/Simulador de caixa eletrônico.py
@@ -1,7 +1,24 @@
-contador = 0
-print('---SIMULADOR DE CAIXA ELETRÔNICO---')
-cedulas = [2, 5, 10, 20, 50, 100]
+valor = int(input('Digite quanto deseja sacar: R$'))
+total = valor
+real = 50
+qnt_real = 0
 while True:
-    valor = int(input('Qual o valor você deseja sacar?'))
-
-print('Helo worlad
+    if valor <=0:
+        print('Saque ínvalido !!!')
+        break
+    if total>=real:
+        total-=real
+        qnt_real+=1
+    else:
+        if qnt_real>0:
+            print(f'Entregue {qnt_real} notas de R${real}')
+        if total ==0:
+            break
+        if real ==50:
+            real = 20
+        elif real == 20:
+            real = 10
+        elif real == 10:
+            real = 1
+        qnt_real = 0
+print('FIM DO PROGRAMA')
