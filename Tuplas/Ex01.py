40669601a92a71f0be0716f367f484a6628d3f7b
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-07-05 15:44:27 -0300

    guy

---
+++ Tuplas/Ex01.py
@@ -0,0 +1,6 @@
+#TUPLAS SÃO IMUTÁVEIS
+extenso = ('zero', 'um', 'dois','três','Quatro','Cinco', 'seis','sete','oito','nove','dez','onze','doze','treze','quatorze','quinze','dezeseis','dezesete','dezoito','dezenove','vinte')
+num = int(input('Qual número você deseja escrever por extenso ? [0-20]: '))
+
+While
+
